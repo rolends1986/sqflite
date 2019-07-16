5cb4358603b6829f935f033fb68e12310eeb9282
Ching <133961963322@163.com>
2019-07-17 03:13:58 +0800

    修改通道

--- sqflite/lib/src/sqflite_impl.dart
+++ sqflite/lib/src/sqflite_impl.dart
@@ -6,13 +6,13 @@ import 'services_impl.dart';
 export 'package:math_sqflite/src/collection_utils.dart';
 export 'package:math_sqflite/src/utils.dart';
 
-const MethodChannel channel = MethodChannel('com.tekartik.math_sqflite');
+
 
 // Temp flag to test concurrent reads
 final bool supportsConcurrency = false;
 
 // Make it async safe for dart 2.0.0-dev28+ preview dart 2
 Future<T> invokeMethod<T>(String method, [dynamic arguments]) async {
-  final T result = await channel.invokeMethod(method, arguments) as T;
+  final T result = await MethodChannel('com.tekartik.math_sqflite').invokeMethod(method, arguments) as T;
   return result;
 }
